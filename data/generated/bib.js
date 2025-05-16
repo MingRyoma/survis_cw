3b513a6eaf2249e8b3b34e9a91e6c35956808262
MingRyoma <alyym29@nottingham.ac.uk>
2025-05-16 07:30:53 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,107 @@
+﻿const generatedBibEntries = {
+    "brown2019superhuman": {
+        "author": "Brown, Noam and Sandholm, Tuomas",
+        "doi": "10.1126/science.aay2400",
+        "journal": "Science",
+        "keywords": "type:application, card_game, poker, imperfect_information, reinforcement_learning",
+        "number": "6456",
+        "pages": "885--890",
+        "title": "Superhuman AI for multiplayer poker",
+        "type": "article",
+        "volume": "365",
+        "year": "2019"
+    },
+    "koivisto2020gamification": {
+        "author": "Koivisto, Jonna and Malik, A",
+        "doi": "10.1093/geront/gnaa047",
+        "journal": "The Gerontologist",
+        "keywords": "type:survey, gamification, older_adults, interaction, motivation",
+        "number": "7",
+        "title": "Gamification for Older Adults: A Systematic Literature Review",
+        "type": "article",
+        "volume": "61",
+        "year": "2020"
+    },
+    "mortazavi2024dda": {
+        "author": "Mortazavi, F and Moradi, H and Vahabie, Abdol-Hossein",
+        "doi": "10.1007/s11042-024-18768-x",
+        "journal": "Multimedia Tools and Applications",
+        "keywords": "type:survey, difficulty_adjustment, dda, player_modeling, game_experience",
+        "title": "Dynamic difficulty adjustment approaches in video games: a systematic literature review",
+        "type": "article",
+        "year": "2024"
+    },
+    "niklaus2019survey": {
+        "author": "Niklaus, Jan and Alberti, Matthias and Pondenkandath, Vinay and Ingold, Rolf and Liwicki, Marcus",
+        "booktitle": "2019 Sixth International Conference on Software Defined Systems (SDS)",
+        "doi": "10.1109/SDS.2019.00-12",
+        "keywords": "type:survey, card_game, jass, ai_strategy, rule_based",
+        "organization": "IEEE",
+        "title": "Survey of Artificial Intelligence for Card Games and Its Application to the Swiss Game Jass",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "shade2024alexa": {
+        "author": "Shade, M and Yan, C and Jones, V and Boron, J",
+        "doi": "10.2196/64763",
+        "journal": "JMIR Formative Research",
+        "keywords": "type:application, voice_assistant, usability, older_adults, engagement",
+        "title": "Evaluating Older Adults\u2019 Engagement and Usability with AI Driven Interventions: A Pilot Study (Preprint)",
+        "type": "article",
+        "year": "2024"
+    },
+    "shao2019survey": {
+        "author": "Shao, Kun and Tang, Zheng and Zhu, Yujie and Li, Ning and Zhao, Deyu",
+        "doi": "10.48550/arxiv.1912.10944",
+        "journal": "arXiv preprint arXiv:1912.10944",
+        "keywords": "type:survey, deep_rl, game_ai, training_efficiency, exploration",
+        "title": "A Survey of Deep Reinforcement Learning in Video Games",
+        "type": "article",
+        "year": "2019"
+    },
+    "sugiyanto2021big2": {
+        "author": "Sugiyanto, G and Tai, W-K",
+        "doi": "10.3390/app11094206",
+        "journal": "Applied Sciences",
+        "keywords": "type:application, card_game, big_two, rule_based, evaluation",
+        "number": "9",
+        "pages": "4206",
+        "title": "A Rule-Based AI Method for an Agent Playing Big Two",
+        "type": "article",
+        "volume": "11",
+        "year": "2021"
+    },
+    "vieira2023sample": {
+        "author": "Vieira, S and Tavares, AR and Chaimowicz, L",
+        "doi": "10.1016/j.entcom.2023.100594",
+        "journal": "Entertainment Computing",
+        "keywords": "type:application, reinforcement_learning, card_game, efficiency, ppo",
+        "pages": "100594",
+        "title": "Towards sample efficient deep reinforcement learning in collectible card games",
+        "type": "article",
+        "volume": "47",
+        "year": "2023"
+    },
+    "yin2023humanai": {
+        "author": "Yin, Qiyue and Yang, J and Huang, KQ and Zhao, MJ and Ni, WC and Liang, B and Huang, Y and Wu, S and Wang, L",
+        "doi": "10.1007/s11633-022-1384-6",
+        "journal": "Machine Intelligence Research",
+        "keywords": "type:survey, general_ai, game_techniques, challenges, architecture",
+        "title": "AI in Human-computer Gaming: Techniques, Challenges and Opportunities",
+        "type": "article",
+        "volume": "20",
+        "year": "2023"
+    },
+    "zhang2022bridge": {
+        "author": "Zhang, X and Liu, W and Lou, L and Yang, F",
+        "doi": "10.3390/s22051877",
+        "journal": "Sensors",
+        "keywords": "type:application, bridge, card_game, explainable_ai, visualization",
+        "number": "5",
+        "pages": "1877",
+        "title": "AI Enabled Bridge Bidding Supporting Interactive Visualization",
+        "type": "article",
+        "volume": "22",
+        "year": "2022"
+    }
+};
